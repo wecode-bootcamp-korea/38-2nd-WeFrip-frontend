9d1c5a8d84b004f47a2bef03b057d8697353f87b
zinukk <slsxpseh54@naver.com>
2022-11-01 14:08:13 +0900

    ADD : Main 컴포넌트 분리 및 뼈대 스타일링 적용 - MainCarousel.js - MainRecentFrip.js - MainCategoryFrip.js - MainSmallBanner.js

---
+++ src/pages/Main/MainCategoryFrip/MainCategoryFrip.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainCategoryFrip = () => {
+  return (
+    <MainCategoryFripContainer>
+      메인 카테고리 프립 위치
+    </MainCategoryFripContainer>
+  );
+};
+
+const MainCategoryFripContainer = styled.div`
+  width: 100%;
+  height: 400px;
+  border: 2px solid black;
+  margin-top: 100px;
+`;
+
+export default MainCategoryFrip;
---
+++ src/pages/Main/MainRecentFrip/MainRecentFrip.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainRecentFrip = () => {
+  return <MainRecentFripContainer>메인 최근 프립 위치</MainRecentFripContainer>;
+};
+
+const MainRecentFripContainer = styled.div`
+  width: 100%;
+  height: 400px;
+  border: 2px solid black;
+  margin-top: 100px;
+`;
+
+export default MainRecentFrip;
