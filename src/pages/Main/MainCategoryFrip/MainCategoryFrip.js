b86f113889f147010021dc670d1ec14cf8ef037f
zinukk <slsxpseh54@naver.com>
2022-11-01 22:32:59 +0900

    ADD : Category Frip UI 구현 완료

--- src/pages/Main/MainCategoryFrip/MainCategoryFrip.js
+++ src/pages/Main/MainCategoryFrip/MainCategoryFrip.js
@@ -1,19 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Product from 'components/Product/Product';
 
 const MainCategoryFrip = () => {
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    fetch(`/data/RecentFrip.json`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    })
+      .then(response => {
+        if (response.ok === true) {
+          return response.json();
+        }
+        throw new Error('에러 발생, check status code');
+      })
+      .then(result => setCategory(result.data.slice(0, 4)))
+      .catch(error => alert(error));
+  }, []);
+
   return (
     <MainCategoryFripContainer>
-      메인 카테고리 프립 위치
+      <CategoryTitleBox>
+        <CategoryTitle>나의 일상을 변화시켜줄 프립</CategoryTitle>
+        <CategorySeeAll>전체 보기</CategorySeeAll>
+      </CategoryTitleBox>
+      <CategoryListBox>
+        {category?.map(product => (
+          <Product key={product.productId} product={product} />
+        ))}
+      </CategoryListBox>
     </MainCategoryFripContainer>
   );
 };
 
 const MainCategoryFripContainer = styled.div`
   width: 100%;
-  height: 400px;
-  border: 2px solid black;
   margin-top: 100px;
+`;
+
+const CategoryTitleBox = styled.div`
+  ${({ theme }) => theme.variables.flex('row', 'space-between', 'center')};
+  width: 1200px;
+  margin: 0 auto;
+`;
+
+const CategoryTitle = styled.h2`
+  margin: 20px 0;
+  color: ${({ theme }) => theme.style.black};
+  font-weight: bold;
+  font-size: 22px;
+  white-space: nowrap;
+  overflow: hidden;
+`;
+
+const CategorySeeAll = styled.p`
+  color: ${({ theme }) => theme.style.deepGrey};
+  font-size: 16px;
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+const CategoryListBox = styled.div`
+  ${({ theme }) => theme.variables.flex('row', 'space-between', 'null')};
+  width: 1200px;
+  margin: 0 auto;
 `;
 
 export default MainCategoryFrip;
