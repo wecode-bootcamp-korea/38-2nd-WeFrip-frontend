24e24d88e08c8b1ea0dc9087639f2dbdbb12e75e
Kyu heum Choi <kyuheumchoi@Kyuui-MacBookPro.local>
2022-11-10 23:17:34 +0900

    FIX : 위시리스트 페이지 위시버튼 로직 수정

--- src/pages/Wishlist/components/Wishlistlist.js
+++ src/pages/Wishlist/components/Wishlistlist.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import Product from 'components/Product/Product';
+import { useState } from 'react';
 
 const Wishlistlist = ({ wished }) => {
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const newProducts = wished.map(product => ({
+    ...product,
+    isBookmarked: true,
+  }));
+
   return (
     <Wrapper>
-      {wished.map(product => (
-        <Product key={product.productId} product={product} />
+      {newProducts.map(product => (
+        <Product
+          key={product.productId}
+          product={product}
+          bookmarks={bookmarks}
+          setBookmarks={setBookmarks}
+        />
       ))}
     </Wrapper>
   );
